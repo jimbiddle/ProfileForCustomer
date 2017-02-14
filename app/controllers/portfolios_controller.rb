3c63532a7025ba375ba0cadb626fcdd37b1de338
Jordan Hudgens <jordan@devcamp.com>
2017-02-14 22:58:26 +0000

    added routes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,10 +7,8 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.new
   end
   
-   def create
+  def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
-
-
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Your Portfolio Item Is Now Live.' }
@@ -38,7 +36,16 @@ class PortfoliosController < ApplicationController
   
   def show
     @portfolio_item = Portfolio.find(params[:id])
-  
   end
+  
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
+    end
+  end
+
   
 end
