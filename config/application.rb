6374448f2b03f6965cc70b5236b0c232cd23597d
jpbiddle@hotmail.com <jordan@devcamp.com>
2017-02-17 22:30:58 +0000

    having an issue

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,7 @@
 require_relative 'boot'
 
+
+
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
@@ -18,6 +20,7 @@ Bundler.require(*Rails.groups)
 
 module DevCampPortfolio
   class Application < Rails::Application
+    config.web_console.whiny_requests = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
